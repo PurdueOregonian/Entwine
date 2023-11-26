ed315c3af80e4eddf1460e3eb3f78390c9f6cb4d
PurdueOregonian <williamliang451@gmail.com>
2023-11-25 17:37:12 -0800

    popup switches from login to register

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
+type LoginPopupMode = 'Login' | 'Register';
+
 function Login() {
+
+    const [displayModal, setDisplayModal] = useState(false);
+    const [loginPopupMode, setLoginPopupMode] = useState<LoginPopupMode>('Login');
+
+    const displayTheModal = () => {
+        setDisplayModal(true);
+        setLoginPopupMode('Login');
+    }
+
     const {
         register,
         handleSubmit
@@ -9,7 +21,7 @@ function Login() {
     const onSubmit = (formData: any) => {
 
         // Define the API URL
-        const apiUrl = 'https://localhost:7253/Login';
+        const apiUrl = `https://localhost:7253/Auth/${loginPopupMode}`;
 
         // Make a GET request
         fetch(apiUrl, {
@@ -31,39 +43,35 @@ function Login() {
             });
     };
 
-    function openModal() {
-        var modal = document.getElementById('loginModal');
-        modal!.style.display = 'flex';
-    }
-
-    function closeModal() {
-        var modal = document.getElementById('loginModal');
-        modal!.style.display = 'none';
-    }
-
     return (
         <>
-            <button id="loginButton" onClick={openModal}>Log In</button>
-            <div id="loginModal" className="modal">
-                <div className="modal-content">
-                    <h2>Login Form</h2>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <label>
-                            Username:
-                            <input {...register("Username")}></input>
-                        </label>
-                        <label>
-                            Password:
-                            <input {...register("Password")}></input>
-                        </label>
-                        <div className="form-control">
-                            <label></label>
-                            <button type="submit">Login</button>
-                            <button onClick={closeModal}>Cancel</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            <button id="loginButton" onClick={() => displayTheModal()}>Log In</button>
+            {displayModal &&
+                <div id="loginModal" className="modal">
+                    <div className="modal-content">
+                        <h2>{loginPopupMode === 'Login' ? 'Login' : 'Register'}</h2>
+                        {loginPopupMode === 'Login' &&
+                            <>
+                                <span>New user?</span><button onClick={() => setLoginPopupMode('Register')}>Register</button>
+                            </>
+                        }
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <label>
+                                Username:
+                                <input {...register("Username")}></input>
+                            </label>
+                            <label>
+                                Password:
+                                <input {...register("Password")}></input>
+                            </label>
+                            <div className="form-control">
+                                <label></label>
+                                <button type="submit">{loginPopupMode === 'Login' ? 'Login' : 'Register'}</button>
+                                <button onClick={() => setDisplayModal(false)}>Cancel</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>}
         </>
     );
 }
