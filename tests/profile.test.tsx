f28d9475e487b9c210b419c6e07607d6801568a1
William Liang <williamliang451@gmail.com>
2024-10-26 22:24:38 -0700

    fix the profile test

--- tests/profile.test.tsx
+++ tests/profile.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, act } from '@testing-library/react';
 import { backendUrl } from '../src/constants/constants';
 import { MemoryRouter as Router } from "react-router-dom";
 import Profile from '../src/pages/Profile';
@@ -8,7 +8,11 @@ jest.mock('../src/api/axios');
 
 describe('Profile component', () => {
     beforeAll(() => {
-        (axiosPrivate.post as jest.Mock).mockResolvedValue({ data: {} });
+        (axiosPrivate.get as jest.Mock).mockResolvedValue({ status: 200, data: {
+            dateOfBirth: '2002-10-04',
+            gender: 'Male'
+        } });
+        (axiosPrivate.post as jest.Mock).mockResolvedValue({ data: { status: 200 } });
         global.fetch = jest.fn(() =>
             Promise.resolve({
                 ok: true
@@ -16,16 +20,19 @@ describe('Profile component', () => {
         ) as jest.Mock;
     });
 
-    beforeEach(() => {
+    beforeEach(async () => {
         jest.clearAllMocks();
-        render(<Router>
-            <Profile />
-        </Router>
-        );
+
+        // necessary to wrap in act because on load, we get the profile data and rerender
+        await act(async () => {
+            render(<Router>
+                <Profile />
+            </Router>);
+        });
     })
 
     test('submits profile', async () => {
-        const monthInput = screen.getByTestId('month');
+        const monthInput = await screen.findByTestId('month');
         fireEvent.change(monthInput, { target: { value: '07' } });
         const dayInput = screen.getByTestId('day');
         fireEvent.change(dayInput, { target: { value: '22' } });
