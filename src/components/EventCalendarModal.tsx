0c9d42a7d35ef93f7f31da2697c77c0a3ccddc09
William Liang <williamliang451@gmail.com>
2025-04-02 10:46:41 -0700

    oops

--- src/components/EventCalendarModal.tsx
+++ src/components/EventCalendarModal.tsx
@@ -47,14 +47,6 @@ const EventCalendarModal = ({ open, setOpen }: EventCalendarModalProps) => {
                 }
             });
     }, [])
-
-    if (loading) return (
-        <div>Loading...</div>
-    );
-
-    if (error) return (
-        <div>{error}</div>
-    );
     
     return (
         <Modal
